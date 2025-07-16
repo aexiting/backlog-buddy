4c26473915046d71d20cf01b80d17396dae447b2
aexiting <adamttway@gmail.com>
2025-07-16 15:49:46 -0400

    Clean up some code related to playwright tests.

--- e2e/playwright.global-setup.ts
+++ e2e/playwright.global-setup.ts
@@ -3,18 +3,9 @@ import { chromium, expect } from '@playwright/test';
 import fs from 'fs/promises';
 
 export default async () => {
-    // const browser = await chromium.launch();
-    // const page = await browser.newPage();
-
-    //
-    //
-    // await page.context().storageState({ path: 'playwright/.auth/user.json' });
-    // await browser.close();
-
     const browser = await chromium.launch();
 
     const page = await browser.newPage();
 
     await page.goto('http://localhost:5173/');
-
 };
